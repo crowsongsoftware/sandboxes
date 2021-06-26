159293474f4cf02f160cba0ad8405189895f85d8
lilmick <mcmahonmichael56@gmail.com>
2021-06-26 17:02:27 -0400

    Added ViewChild project, updated to angular 12.1.0

--- angular/projects/view-child/src/app/app.component.spec.ts
+++ angular/projects/view-child/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular'`, () => {
+  it(`should have as title 'view-child'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular');
+    expect(app.title).toEqual('view-child');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('view-child app is running!');
   });
 });
