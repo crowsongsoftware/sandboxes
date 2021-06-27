f972139f8233dfce6d00b67804698c6ce96686bb
lilmick <mcmahonmichael56@gmail.com>
2021-06-27 13:07:47 -0400

    added parent and child components

--- angular/projects/view-child/src/app/app.module.ts
+++ angular/projects/view-child/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ChildComponentComponent } from './child-component/child-component.component';
+import { ChildComponentComponent } from './parent-component/child-component/child-component.component';
+import { ParentComponentComponent } from './parent-component/parent-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponentComponent
+    ChildComponentComponent,
+    ParentComponentComponent
   ],
   imports: [
     BrowserModule
--- angular/projects/view-child/src/app/parent-component/child-component/child-component.component.ts
+++ angular/projects/view-child/src/app/parent-component/child-component/child-component.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-child-component',
+  selector: 'child-component',
   templateUrl: './child-component.component.html',
   styleUrls: ['./child-component.component.scss']
 })
