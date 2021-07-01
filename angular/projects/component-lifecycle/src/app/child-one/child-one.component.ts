221600eaf45fa1aeccc3dc45042d5abc602583ca
Michael McMahon <git.crowsong@gmail.com>
2021-07-01 09:05:40 -0400

    added components for component-lffecylce

--- angular/projects/component-lifecycle/src/app/child-one/child-one.component.ts
+++ angular/projects/component-lifecycle/src/app/child-one/child-one.component.ts
@@ -1,15 +1,88 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-child-one',
+  selector: 'child-one',
   templateUrl: './child-one.component.html',
   styleUrls: ['./child-one.component.scss']
 })
 export class ChildOneComponent implements OnInit {
+  public propInitDeclaration: string = "Property Initialized at Declaration";
+  public propInitConstructor: string ;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  private beforeInit(lifecycle: string){
+    console.groupCollapsed("ChildOneComponent: beforeInit ", lifecycle);
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
   }
 
-}
+  constructor(){
+    //this.beforeInit("constructor");
+    this.propInitConstructor = "Property Initialized in Constructor";
+    console.groupCollapsed("ChildOneComponent: constructor");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  ngOnChanges(){
+    //this.beforeInit("ngOnChanges");
+    console.groupCollapsed("ChildOneComponent: ngOnChanges");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  ngOnInit(): void {
+    //this.beforeInit("ngOnInit");
+    console.groupCollapsed("ChildOneComponent: ngOnInit");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  // ngOnInit
+  ngDoCheck(){
+    //this.beforeInit("ngDoCheck");
+    console.groupCollapsed("ChildOneComponent: ngDoCheck");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  // ngOnInit -> ngDoCheck
+  ngAfterContentInit(): void {
+    //this.beforeInit("ngAfterContentInit");
+    console.groupCollapsed("ChildOneComponent: ngAfterContentInit");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  // ngOnInit -> ngDoCheck -> ngAfterContentInit
+  // ngOnInit -> ngDoCheck ->  ngAfterViewInit
+  ngAfterContentChecked(): void {
+    //this.beforeInit("ngAfterContentChecked");
+    console.groupCollapsed("ChildOneComponent: ngAfterContentChecked");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  // ngOnInit -> ngDoCheck -> ngAfterContentInit -> ngAfterContentChecked
+  ngAfterViewInit(): void {
+    //this.beforeInit("ngAfterViewInit");
+    console.groupCollapsed("ChildOneComponent: ngAfterViewInit");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  ngOnDestroy(){
+    //this.beforeInit("ngOnDestroy");
+    console.groupCollapsed("ChildOneComponent: ngOnDestroy");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+};
--- angular/projects/component-lifecycle/src/app/grandchild-one/grandchild-one.component.ts
+++ angular/projects/component-lifecycle/src/app/grandchild-one/grandchild-one.component.ts
@@ -1,15 +1,88 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-grandchild-one',
+  selector: 'grandchild-one',
   templateUrl: './grandchild-one.component.html',
   styleUrls: ['./grandchild-one.component.scss']
 })
 export class GrandchildOneComponent implements OnInit {
+  public propInitDeclaration: string = "Property Initialized at Declaration";
+  public propInitConstructor: string ;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  private beforeInit(lifecycle: string){
+    console.groupCollapsed("GrandChildOneComponent: beforeInit ", lifecycle);
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
   }
 
-}
+  constructor(){
+    //this.beforeInit("constructor");
+    this.propInitConstructor = "Property Initialized in Constructor";
+    console.groupCollapsed("GrandChildOneComponent: constructor");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  ngOnChanges(){
+    //this.beforeInit("ngOnChanges");
+    console.groupCollapsed("GrandChildOneComponent: ngOnChanges");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  ngOnInit(): void {
+    //this.beforeInit("ngOnInit");
+    console.groupCollapsed("GrandChildOneComponent: ngOnInit");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  // ngOnInit
+  ngDoCheck(){
+    //this.beforeInit("ngDoCheck");
+    console.groupCollapsed("GrandChildOneComponent: ngDoCheck");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  // ngOnInit -> ngDoCheck
+  ngAfterContentInit(): void {
+    //this.beforeInit("ngAfterContentInit");
+    console.groupCollapsed("GrandChildOneComponent: ngAfterContentInit");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  // ngOnInit -> ngDoCheck -> ngAfterContentInit
+  // ngOnInit -> ngDoCheck ->  ngAfterViewInit
+  ngAfterContentChecked(): void {
+    //this.beforeInit("ngAfterContentChecked");
+    console.groupCollapsed("GrandChildOneComponent: ngAfterContentChecked");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  // ngOnInit -> ngDoCheck -> ngAfterContentInit -> ngAfterContentChecked
+  ngAfterViewInit(): void {
+    //this.beforeInit("ngAfterViewInit");
+    console.groupCollapsed("GrandChildOneComponent: ngAfterViewInit");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+
+  ngOnDestroy(){
+    //this.beforeInit("ngOnDestroy");
+    console.groupCollapsed("GrandChildOneComponent: ngOnDestroy");
+    console.log("propInitDeclaration", this.propInitDeclaration);
+    console.log("propInitConstructor", this.propInitConstructor);
+    console.groupEnd;
+  };
+};
